9611d462a6d6f6233ef1d99d90a21ba0a4aaefba
ZenWar0 <desmondsoo89@1utar.my>
2024-08-08 00:49:36 +0800

    CI system to automate build and test procedures.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,16 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../renderer/App';
 
 describe('App', () => {
   it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
+    render(<App />);
+    
+    // Update to match the stubbed value
+    expect(screen.getByAltText('icon')).toHaveAttribute('src', 'test-file-stub');
+    expect(screen.getByText('electron-react-boilerplate')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /Read our docs/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /Read our docs/i })).toHaveAttribute('href', 'https://electron-react-boilerplate.js.org/');
+    expect(screen.getByRole('link', { name: /Donate/i })).toHaveAttribute('href', 'https://github.com/sponsors/electron-react-boilerplate');
   });
 });
